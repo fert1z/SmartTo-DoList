342189d23b76b318a4003c6bc48bbc996ab0bf2c
fert1z <164030064+fert1z@users.noreply.github.com>
2026-03-18 21:02:48 +0300

    login

---
+++ static/js/login.js
@@ -0,0 +1,22 @@
+function validLogin(event) {
+    return validatePassword(event) && validateUsername(event);
+}
+
+
+
+
+function validatePassword(event) {
+    var password = document.querySelector("#password").value;
+}
+
+function validateUsername(event) {
+    var username = document.querySelector("#username").value;
+    if (username.length < 5){
+        alert("Username must be at least 5 characters long.");
+        if (event) {
+            event.preventDefault();
+        }
+        return false;   
+    }
+    return true;
+}
