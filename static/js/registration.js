7b9040d651f3a390a9e4ec5ce8658f710132009f
fert1z <164030064+fert1z@users.noreply.github.com>
2026-03-29 23:37:47 +0300

    all commit

---
+++ static/js/registration.js
@@ -0,0 +1,76 @@
+function validForm(event) {
+    return confirmPasswords(event) && UsernameValidation(event) && validatepassword(event);
+}
+
+
+
+
+
+function validatepassword(event) {
+    var password = document.querySelector("#password").value;
+    var regex = /[@#$%&!/]/;
+    var notregex = /[+=-_({})*^;:,.?"'| ]/;
+
+    if (!regex.test(password) ) {
+        alert("Password must contain at least one special character (@, #, $, %, &, !, /)");
+        if (event) {
+            event.preventDefault();
+        }
+        return false; 
+    }
+
+    if (notregex.test(password)) {
+        alert("Password cannot contain the following characters: +, =, -, _, (, {, }, ), *, ^, ;, :, ,, ., ?, \", ', |");
+        if (event) {
+            event.preventDefault();
+        }
+        return false;
+    }
+    
+    if (password.length < 8){
+        alert("Password must be at least 8 characters long!");
+        if (event) {
+            event.preventDefault();
+        }
+        return false;
+    }
+
+    return true;
+}
+
+function valideEmail(event) {
+    var email = document.querySelector("#email").value;
+}
+
+
+function confirmPasswords(event) {
+
+    var password = document.querySelector("#password").value;
+    var confirmPassword = document.querySelector("#confirm_password").value;
+
+
+    if (password != confirmPassword){
+        alert("Passwords do not match!");
+        if (event) {
+            event.preventDefault();
+        }
+        return false;
+    }
+
+    return true;
+} 
+
+function UsernameValidation(event) {
+
+    var username = document.querySelector("#username").value;
+
+    if (username.length < 6){
+        alert("Username must be at least 6 characters long!");
+        if (event) {
+            event.preventDefault();
+        }
+        return false;
+    }
+
+    return true;
+}
